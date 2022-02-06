645e3dcc7c85daddd7e0122cf49ee15f09cecbc8
Just-Mmathapelo <98430360+Just-Mmathapelo@users.noreply.github.com>
2022-02-06 09:58:46 +0200

    Update and rename README.md to Task0.9.js

---
+++ Task0.9.js
@@ -0,0 +1,14 @@
+
+function vowelCount(str) {
+    var count = 0;
+    var vowels = "aeiou";
+    
+    for(var i = 0; i <str.lenght; i ++){
+        if (vowels.indexOfStr[i].toLowerCase()) > -1) {
+        count++;
+    }
+}
+ 
+     return "This string has " + count + " vowels in it";
+}
+vowelCount("Umuzi");
