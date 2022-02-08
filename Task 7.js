cc1ceb5d180150bd8f3f9e663a1f9b879faef212
Just-Mmathapelo <98430360+Just-Mmathapelo@users.noreply.github.com>
2022-02-08 22:37:44 +0200

    Add files via upload

---
+++ Task 7.js
@@ -0,0 +1,8 @@
+//celcius to fahrenheit
+function converCelciusToFahrenheit(celsius){
+     var cTemp = celsius;
+      var cToFahr = cTemp * 9 / 5 +32; 
+      return cToFahr; } console.log(converCelciusToFahrenheit(28)); 
+//fahrenheit to celcius
+function convertFahrenheitToCelcius(fahrenheit{ let celcius = fahrenheit*5/9 -32;
+   return celcius; } console.log(convertFahrenheitToCelcius(28));
