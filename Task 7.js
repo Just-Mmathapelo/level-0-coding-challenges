2b838ca2a478b235819c157ccfcc4a472b1d7e8b
Just-Mmathapelo <98430360+Just-Mmathapelo@users.noreply.github.com>
2022-02-08 22:38:58 +0200

    Update Task 7.js

--- Task 7.js
+++ Task 7.js
@@ -2,7 +2,9 @@
 function converCelciusToFahrenheit(celsius){
      var cTemp = celsius;
       var cToFahr = cTemp * 9 / 5 +32; 
-      return cToFahr; } console.log(converCelciusToFahrenheit(28)); 
+      return cToFahr; } 
+console.log(converCelciusToFahrenheit(28)); 
 //fahrenheit to celcius
 function convertFahrenheitToCelcius(fahrenheit{ let celcius = fahrenheit*5/9 -32;
-   return celcius; } console.log(convertFahrenheitToCelcius(28));
+   return celcius; } 
+console.log(convertFahrenheitToCelcius(28));
