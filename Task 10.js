1805d1bd72a0b3887d62b90bce969843a0d726ab
Just-Mmathapelo <98430360+Just-Mmathapelo@users.noreply.github.com>
2022-02-09 02:15:34 +0200

    Add files via upload

---
+++ Task 10.js
@@ -0,0 +1,8 @@
+
+const commonCharacters = function(string1, string2) { 
+  let duplicateCharacter = ''; 
+  for (let i = 0; i < string1.length; i += 1) { 
+  if (duplicateCharacter.indexOf(string1[i]) === -1) { 
+  if (string2.indexOf(string1[i]) !== -1) { duplicateCharacter += string1[i]; } } }
+   return duplicateCharacter;
+ }; console.log(commonCharacters('house','computers'))
---
+++ Task 9.js
@@ -0,0 +1,4 @@
+"use strict" 
+ let s = "Umuzi" 
+ let c, i for (i=0; i<=s.length-1; i++){ c = s[i] 
+ if (c=="a"|| c=="e" || c=="i" || c=="o" || c=="u" || c=="A"|| c=="E"|| c=="I" || c=="O" || c=="U"){ console.log(c) } }
