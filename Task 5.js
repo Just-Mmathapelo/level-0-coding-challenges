9ee7daa64bb02f3dc3957458e9d8d34935b7eb78
Just-Mmathapelo <98430360+Just-Mmathapelo@users.noreply.github.com>
2022-02-09 20:08:26 +0200

    Update Task 5.js

--- Task 5.js
+++ Task 5.js
@@ -1,5 +1,11 @@
-function areaOfTriangle(a, b, c){
-    var s = (a + b +c)/2;
-    var area = Math.sqrt(s*((s - a)*(s - b)*(s - c)));
-    console.log(area);
+function areaOfTriangle(side1, side2, side3) {
+  let semiP = 0;
+  let area = 0;
+  semiP = 0.5 * (side1 + side2 + side3); //semiperimeter
+
+  area = Math.sqrt(semiP * (semiP - side1) * (semiP - side2) * (semiP - side3));
+
+  return area;
 }
+
+console.log(areaOfTriangle(10, 12, 5));
