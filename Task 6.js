b60cbb42ea58d13c7100433bc766851d6ae711b8
Just-Mmathapelo <98430360+Just-Mmathapelo@users.noreply.github.com>
2022-02-10 07:12:56 +0200

    Update Task 6.js

--- Task 6.js
+++ Task 6.js
@@ -1,11 +1,13 @@
-  function max_of_three(x, y, z) { max_val = 0; 
-  if (x > y) {
-       max_val = x; 
+  function maxOfThree(x, y, z) {
+       maxVal = 0; 
+       if (x > y) {
+       maxVal = x; 
        
-       } else { max_val = y; } 
-       if (z > max_val) { max_val = z; } 
-       return max_val; 
+       } else { maxVal = y; } 
+       if (z > maxVal) { maxVal = z; } 
+       return maxVal; 
        
 } 
 
-console.log(max_of_three(10,0,1)); console.log(max_of_three(0,-100,0.5)); 
+console.log(maxOfThree(10,0,1));
+console.log(maxOfThree(0,-100,0.5)); 
