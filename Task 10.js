10cfefc32d4109a181e799ec3bd89e441206eb49
Just-Mmathapelo <98430360+Just-Mmathapelo@users.noreply.github.com>
2022-02-14 13:22:52 +0200

    Update Task 10.js

--- Task 10.js
+++ Task 10.js
@@ -1,3 +1,4 @@
+
 function commonCharacters(inputString1, inputString2) {
   inputString1 = inputString1.toLowerCase();
   inputString2 = inputString2.toLowerCase();
@@ -10,7 +11,7 @@ function commonCharacters(inputString1, inputString2) {
   let bagOfAlphabets = "";
   for (let i = 0; i < inputString1.length; i++) {
     for (let j = 0; j < inputString2.length; j++) {
-      if (inputString1[i] == inputString2[j]) {
+      if (inputString1[i] === inputString2[j]) {
         //add letter to bag of alphabets
         bagOfAlphabets = bagOfAlphabets + inputString2[j];
       } else {
@@ -40,7 +41,7 @@ function removeDuplicate(string) {
   return string
     .split("")
     .filter(function (item, pos, self) {
-      return self.indexOf(item) == pos;
+      return self.indexOf(item) === pos;
     })
     .join("");
 }
